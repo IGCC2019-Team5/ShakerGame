4705b904be44d585ac2696c53e3dcfe095f4ba5d
Fatin <fatin.atiqah.yusof@gmail.com>
2019-09-04 03:43:45 +0800

    Created animations for BackGroundSize And Garlic sprite
    BackGround Size to Zoom In and ZoomOut when it called from a function use instance.
    Orignal BackGround: Pos<0,-0.8,0>,Scale<1,1,1> .ZoomIn:Position<0,2.6,0>,Scale<0.6,0.6,1>.

---
+++ Assets/Scripts/ChangeBGSize.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeBGSize : MonoBehaviour
+{
+    public static ChangeBGSize instance = null;
+    Animator m_Animator;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = null;
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+
+    public void ZoomOutBG()
+    {
+        m_Animator.SetTrigger("ZoomOut");
+    }
+    public void ZoomInBG()
+    {
+
+        m_Animator.SetTrigger("ZoomIn");
+    }
+    public static void Destory()
+    {
+        if (instance)
+            Destroy(instance.gameObject);
+    }
+}
