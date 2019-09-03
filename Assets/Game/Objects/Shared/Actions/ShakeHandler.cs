360b08bb48cc9d35450f29b0a0281690d1b74d7b
ydeagames <ydeagames@gmail.com>
2019-09-03 11:32:43 +0800

    Title

---
+++ Assets/Game/Objects/Shared/Actions/ShakeHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ShakeHandler : MonoBehaviour
+{
+    public UnityEvent action = new UnityEvent();
+
+    ActionOnce actionOnce;
+    public float shakeMagnitude = 1;
+
+    void Start()
+    {
+        actionOnce = new ActionOnce(action);
+    }
+
+    void Update()
+    {
+        if (Input.gyro.userAcceleration.magnitude > shakeMagnitude)
+            actionOnce.Invoke();
+    }
+}
