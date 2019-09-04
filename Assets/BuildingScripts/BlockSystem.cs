948f0bee9506c20865700f5e4889a79d67b87d3d
ydeagames <ydeagames@gmail.com>
2019-09-04 13:27:50 +0800

    Grid System

--- Assets/BuildingScripts/BlockSystem.cs
+++ Assets/BuildingScripts/BlockSystem.cs
@@ -7,31 +7,31 @@ using UnityEngine;
 public class BlockSystem : MonoBehaviour
 {
     // Array we expose to inspector / editor, use this instead of the old arrays to define block types.
-    [SerializeField]
-    [Obsolete]
-    private BlockType[] allBlockTypes;
+    //[SerializeField]
+    //[Obsolete]
+    //private BlockType[] allBlockTypes;
     public BlocksPalette blockTypes;
 
     // Array to store all blocks created in Awake()
-    [HideInInspector]
-    public Block[] allBlocks;
+    //[HideInInspector]
+    //public Block[] allBlocks;
 
 
     private void Awake()
     {
-        // Initialise allBlocks array.
-        allBlocks = new Block[allBlockTypes.Length];
+        //// Initialise allBlocks array.
+        //allBlocks = new Block[allBlockTypes.Length];
 
 
 
-        // For loops to populate main allBlocks array.
-        for (int i = 0; i < allBlockTypes.Length; i++)
-        {
-            // Instead of referencing multiple arrays, we just create a new BlockType object and get values from that.
-            BlockType newBlockType = allBlockTypes[i];
-            allBlocks[i] = new Block(i, newBlockType.blockName, newBlockType.blockSprite, newBlockType.blockIsSolid);
-            Debug.Log("Solid block: allBlocks[" + i + "] = " + newBlockType.blockName);
-        }
+        //// For loops to populate main allBlocks array.
+        //for (int i = 0; i < allBlockTypes.Length; i++)
+        //{
+        //    // Instead of referencing multiple arrays, we just create a new BlockType object and get values from that.
+        //    BlockType newBlockType = allBlockTypes[i];
+        //    allBlocks[i] = new Block(i, newBlockType.blockName, newBlockType.blockSprite, newBlockType.blockIsSolid);
+        //    Debug.Log("Solid block: allBlocks[" + i + "] = " + newBlockType.blockName);
+        //}
     }
 
 
@@ -42,29 +42,25 @@ public class BlockSystem : MonoBehaviour
 [Serializable]
 public class Block
 {
-    public int blockID;
-    public string blockName;
-    public Sprite blockSprite;
+    public int id;
     public bool isSolid;
 
-    public Block(int id, string myName, Sprite mySprite, bool amISolid)
+    public Block(int id, bool isSolid)
     {
-        blockID = id;
-        blockName = myName;
-        blockSprite = mySprite;
-        isSolid = amISolid;
+        this.id = id;
+        this.isSolid = isSolid;
     }
 }
 
 // Custom struct for Block type.
-[Serializable]
-public struct BlockType
-{
-    // Main, differing variables for each block type.
-    public string blockName;
-    public Sprite blockSprite;
-    public bool blockIsSolid;
-}
+//[Serializable]
+//public struct BlockType
+//{
+//    // Main, differing variables for each block type.
+//    public string blockName;
+//    public Sprite blockSprite;
+//    public bool blockIsSolid;
+//}
 
 
 
